7d34c3f4a64beebce5921869c9927b300b1bbf28
pragmatic-memory-1672 <hp.mars.le@gmail.com>
2019-07-24 16:36:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -67,7 +67,9 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do
-    if source_array[i]
+    if !source_array[i]
+    end
+  end 
 #returns true when all values are truthy and
 
 #returns flase when any value is false 
